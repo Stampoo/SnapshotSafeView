16a45ce1420dbab1ed123d4c57f815a98e1ebd10
ilya <fivecoil@gmail.com>
2023-01-31 18:26:52 +0300

    feature(flutter): Adding `SnapshotSafeViewFlutterInterceptor` for injecting to `FlutterViewController`

---
+++ Sources/SnapshotSafeView/RootControllerInterceptor/RootControllerExpectation/Flutter/FlutterExpectation.swift
@@ -0,0 +1,16 @@
+//
+//  FlutterExpectation.swift
+//  
+//
+//  Created by Илья Князьков on 30.01.2023.
+//
+
+import Foundation
+
+public struct FlutterExpectation: RootControllerExpectation {
+
+    public var typeInStringRepresentation: String = "FlutterViewController"
+
+    public init() { }
+
+}
